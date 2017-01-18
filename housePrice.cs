6ff839852932a1d7f21a2b574e9562af3817be06
SirChocho <sdborisov@yahoo.com>
2017-01-18 22:28:55 +0200

    HousePrice

---
+++ housePrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace housePrice
+{
+    class housePrice
+    {
+        static void Main(string[] args)
+        {
+            double smallerRoom = double.Parse(Console.ReadLine());
+            double kitchen = double.Parse(Console.ReadLine());
+            double pricePerSqrtM = double.Parse(Console.ReadLine());
+
+            double bathroom = smallerRoom / 2.00;
+            double middleRoom = (smallerRoom / 10.00) + smallerRoom;
+            double biggestRoom = (middleRoom / 10.00) + middleRoom;
+
+            double house = (smallerRoom + kitchen + bathroom + middleRoom + biggestRoom);
+            house = (house / 20.00) + house;
+            double price = house * pricePerSqrtM;
+            Console.WriteLine("{0:f2}", price);
+        }
+    }
+}
